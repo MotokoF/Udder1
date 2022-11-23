f86000fced483fd581d3b0e2f558a2769f71fa28
MotokoF <furukirufu@gmail.com>
2022-11-23 11:20:47 +0900

    Initial Commit

---
+++ Udder1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder1
+//
+//  Created by Motoko Furuki on 2022/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
